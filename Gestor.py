a8210ff82f4bf9b6fe8829f3fbd42c8fe3af80e6
carlos11calderon <2823890830101@ingenieria.usac.edu.gt>
2021-11-11 14:42:07 -0600

    Complete

--- Gestor.py
+++ Gestor.py
@@ -1,18 +1,12 @@
 
-from logging import NullHandler
-from os import stat
-import re
 import mysql.connector
 import json
 import math, statistics
 from werkzeug.wrappers import request
 
 
-
-
-
 class Gestor:
-    def __init__(self):
+    def __init__(self):#connection of the python with mysql database
         self.connection = mysql.connector.connect(user='root', password='Augusto11cal', host='localhost', database='melp', port='3306')
         self.midb = self.connection.cursor() 
     
@@ -28,13 +22,13 @@ class Gestor:
            json_data.append(dict(zip(field_names, i)))
         return json.dumps(json_data)
 
-    
+    ## insert a new register in the data base restaurants
     def InsertRegister(self, iid, rating, name, site, email, phone, street, city, state, lat, lng ):
         insert = "INSERT INTO restaurants(id, rating, name, site, email, phone, street, city, state, lat, lng) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(iid, rating, name, site, email, phone, street, city, state, lat, lng)
         self.midb.execute(insert)
-        self.connection.commit()
+        self.connection.commit()#save the changes in the database
         
-    def updateRegister(self, id,nameN, emailN, phoneN):
+    def updateRegister(self, id,nameN, emailN, phoneN):# update register in the data base in the fields name, email, phone
         update1 = "UPDATE restaurants SET name='{}' WHERE id='{}'".format(nameN, id)
         update2 = "UPDATE restaurants SET email='{}' WHERE id='{}'".format(emailN, id)
         update3 = "UPDATE restaurants SET phone='{}' WHERE id='{}'".format(phoneN, id)
@@ -43,45 +37,44 @@ class Gestor:
         self.midb.execute(update3)
         self.connection.commit()
 
-    def DeleteRegister(self, id):
+    def DeleteRegister(self, id):# function that delete register in the data base
         delete = "DELETE FROM restaurants WHERE id='{}'".format(id)
         self.midb.execute(delete)
         self.connection.commit()
 
     def Alredy(self, lat1, lng1, radius):
         listaR=[]
-        radius= float(radius)
-        lati = "SELECT lat FROM restaurants"
+        radius= float(radius)#parse to float
+        lati = "SELECT lat FROM restaurants"#consulta
         self.midb.execute(lati)
         lats = self.midb.fetchall()
-        longi = "SELECT lng FROM restaurants"
+        longi = "SELECT lng FROM restaurants"#consulta
         self.midb.execute(longi)
         longs = self.midb.fetchall()
         i=0
-        while(i<len(lats)):
+        while(i<len(lats)):#ciclo para recorrer todos los registros
             lat2=lats[i]
             lat2 = lat2[0]
             lng2=longs[i]
             lng2= lng2[0]
-            print("latitud: "+str(lat2)+" long: "+str(lng2))
+            #calculamos la distancia 
             distance = round(self.Calculos(float(lat1), float(lng1), float(lat2),float(lng2)),2)
-            if distance<=radius:
-                print(radius)
-                print("DISTANCIA ES: "+str(distance))
+            if distance<=radius:#determinamos si pertenece al circulo
                 rating = self.GetRating(lat2,lng2)
-                if rating is not None:
+                if rating is not None:#agregamos a la lista rating
                     listaR.append(rating)
             i+=1
         if len(listaR)!=0:
             count = len(listaR)
-            ratingProm = round((sum(listaR)/count),2)#redondeo el promedio a 2 decimales
+            ratingProm = round((sum(listaR)/count),2)#round the prom to 2 decimal
             ratingdesv = round(statistics.pstdev(listaR),2)#redondeo la desviacion a 2 decimales
             resultado='{"Count":"'+str(count)+'", "avg":"'+str(ratingProm)+'", "std":"'+str(ratingdesv)+'"}'
             return resultado
-        return '{"No se pudo"}'
+        return '{"No exist restaurants alrady, try with other radius"}'
         
+    ##in this function, we calculate the distance from center to restaurant     
     def Calculos(self,lat1, lng1, lat2,lng2 ):
-        
+        ## is the coordinates are equals, return 0 in distance
         if lat1 == lat2 and lng1 == lng2:
             return 0
         else:
@@ -90,15 +83,15 @@ class Gestor:
             D = math.acos(a + b)
             distance = 111.18*math.degrees(D)*1000
             return distance
-    
+    # get rating with parameters of coordinate that meet inside radius
     def GetRating(self, lat2,lng2):
         Orating = "SELECT rating FROM restaurants WHERE lat LIKE '{}' AND lng LIKE '{}'".format(lat2,lng2)
-        print(Orating)
         self.midb.execute(Orating)
         rating = self.midb.fetchall()
+        
         if len(rating)!=0:
-            rating = rating[0]#obtengo la primera tupla
-            rating = rating[0]#obtengo el primer dato de la tupla
-            return int(rating)# retorno el rating tipo int
+            rating = rating[0]#get firts tupla
+            rating = rating[0]#get first data of the tupla
+            return int(rating)# return the rating type int
         else: 
             return None
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import *
 from flask_cors import CORS
 from Gestor import Gestor
-import mysql.connector
 
 gestor=Gestor()
 
@@ -12,33 +11,35 @@ app.config["DEBUG"] = True
 def home():
     return 'SERVER IS WORKING!!!!'
 
-@app.route('/MostrarDatos', methods=['GET'])
+@app.route('/ViewData', methods=['GET'])#view data method READ
 def getData():
     return gestor.ViewRegisters()
 
-@app.route('/CreateRegister', methods=['POST'])
+@app.route('/CreateRegister', methods=['POST'])#method CREATE
 def insertRegister():
     data = request.json
     gestor.InsertRegister(data['id'], data['rating'], data['name'], data['site'], data['email'], data['phone'], data['street'], data['city'], data['state'], data['lat'], data['lng'])
     return '{"Estado":"Registro creado"}'
 
-@app.route('/UpdateRegister/<id>', methods=['PUT'])
+@app.route('/UpdateRegister/<id>', methods=['PUT'])#method UPDATE
 def UpdateRegister(id):
     data = request.json
     print(data)
     gestor.updateRegister(id,data['name'],data['email'], data['phone'])
     return '{"Estado":"Registro actualizado"}'
 
-@app.route('/Delete', methods=['DELETE'])
+@app.route('/Delete', methods=['DELETE'])#method DELETE
 def DeleteRegister():
     data = request.json
     gestor.DeleteRegister(data['id'])
     return '{"Estado":"Registro Eliminado"}'
 
-@app.route('/Alredy/<latitud>/<longitud>/<radius>', methods=['GET'])
-def Alredy(latitud, longitud, radius):
-    data = request.json
-    resultado=gestor.Alredy(latitud,longitud,radius)
+@app.route('/Alredy/statistics', methods=['GET'])
+def Alredy():
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    radius = request.args.get('radius')
+    resultado=gestor.Alredy(latitude,longitude,radius)
     return resultado
     
 
